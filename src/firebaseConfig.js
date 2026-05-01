9a2f66f7fcb9f54549363c5058cecec84aca4009
HarshBhushanD <harshbd12@gmail.com>
2026-05-01 20:37:57 +0530

    auth

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -44,3 +44,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
